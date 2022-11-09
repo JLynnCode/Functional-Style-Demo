3bcaa5f4c186e43dc4f39cc2059cb228df12acec
gazgiz <jlynnmail@gmail.com>
2022-11-08 18:34:42 -0600

    Passing all tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 export function calculateTotal (items, tax) {
-  // TODO
+  let totalPrice = 0;
+  let taxedItemPrice = 0;
+  let absoluteTax = Math.abs(tax);
+
+  for(var i = 0; i < items.length; i++){
+
+    if(items[i].taxable == true){
+
+      taxedItemPrice = items[i].price + (items[i].price * absoluteTax);
+      totalPrice += taxedItemPrice;
+    }else{
+
+      totalPrice += items[i].price;
+    }
+  }
+
+  return totalPrice;
 }
